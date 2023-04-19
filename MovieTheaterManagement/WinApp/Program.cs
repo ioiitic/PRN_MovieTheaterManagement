a1f4cb6662cef2248246c80da20b56b066b682a4
Gia Qui <lhgia.huyy@gmail.com>
2023-04-19 16:45:40 +0700

    restore

--- MovieTheaterManagement/WinApp/Program.cs
+++ MovieTheaterManagement/WinApp/Program.cs
@@ -17,7 +17,7 @@ namespace WinApp
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new MainFormGuest());
         }
     }
 }
